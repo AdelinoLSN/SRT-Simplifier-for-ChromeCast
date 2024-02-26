c8fac66e4c8f918fcc22ae1b1987365d550e346d
Adelino Lourenço <adelinolsn@gmail.com>
2024-02-26 15:59:55 -0300

    Changed main function to run a loop instead of unique execution

--- src/file.rs
+++ src/file.rs
@@ -1,5 +1,21 @@
 use std::fs;
 
+pub fn get_srt_file_names(directory: String) -> Vec<String> {
+    let paths = fs::read_dir(directory).unwrap();
+
+    let mut files: Vec<String> = Vec::new();
+
+    for path in paths {
+        let path = path.unwrap().path();
+        let filename = path.file_name().unwrap().to_str().unwrap().to_string();
+        if filename.ends_with(".srt") {
+            files.push(filename);
+        }
+    }
+
+    return files;
+}
+
 pub fn get_file_contents(path: String) -> String {
     let content = fs::read_to_string(path)
         .expect("Something went wrong reading the file");
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 mod subtitle;
 mod file;
 mod time;
-use subtitle::*;
 
 fn main() {
-    let input_file = String::from("files/input/test.srt");
-    let simplified_file = String::from("files/simplified/test.srt");
-    let output_file = String::from("files/output/test.srt");
+    let input_directory = String::from("files/input");
+    let simplified_directory = String::from("files/simplified");
+    let output_directory = String::from("files/output");
 
-    simplify_srt(input_file.clone(), simplified_file.clone());
+    let files = file::get_srt_file_names(input_directory.clone());
+    // Filter files removing the files test.srt and .gitkeep
+    let files = files
+        .iter()
+        .filter(|file| file != &&String::from("files/input/test.srt"))
+        .collect::<Vec<&String>>();
 
-    merge_subtitles(simplified_file.clone(), output_file.clone());
+    for file in files {
+        let input_file = format!("{}/{}", input_directory.clone(), file);
+        let simplified_file = format!("{}/{}", simplified_directory.clone(), file);
+        let output_file = format!("{}/{}", output_directory.clone(), file);
+
+        subtitle::simplify_srt(input_file.clone(), simplified_file.clone());
+        subtitle::merge_subtitles(simplified_file.clone(), output_file.clone());
+    }
 }
